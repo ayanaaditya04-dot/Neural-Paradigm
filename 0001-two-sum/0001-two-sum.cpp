6739c870df73857ce01daf3ad0ac4045189c9de3
Ayan Kumar Aditya <ayanaaditya04@gmail.com>
2026-06-23 12:21:05 +0530

    Time: 1 ms (76.56%), Space: 15 MB (18.76%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // In unsorted array - Two pointer changes the index possition;
+        // Hash map is the most optimal solution
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            // look in hashmap does any pair of complement exist?
+            if (numMap.count(complement)) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i; // store the value of current "i"
+        }
+
+        return {}; // No solution found
+    }
+};
